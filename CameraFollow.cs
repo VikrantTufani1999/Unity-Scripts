ef0979940146ff3f62066f2f14c4a96c84c1db3e
Vikrant Tufani <8052007294vicky@gmail.com>
2020-08-02 21:58:26 +0530

    Camera follow script added

---
+++ CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿// Used in games like doodle jump
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	// Declarations
+    public Transform target;
+
+	
+    void LateUpdate()
+    {
+            Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            transform.position = newPos;
+    }
+}
