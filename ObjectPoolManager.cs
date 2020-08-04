248a266975c390d293ab038803f3d0f3541db547
Vikrant Tufani <8052007294vicky@gmail.com>
2020-08-04 21:22:01 +0530

    Object Pooling basic script

---
+++ ObjectPoolManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    [SerializeField] GameObject poolObj;
+    [SerializeField] int poolAmount;
+
+    List<GameObject> pooledObjects;
+
+    [SerializeField] float minmY;
+    [SerializeField] float maxmY;
+    [SerializeField] float width;
+
+
+
+    private void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        Vector3 spawnpos = new Vector3(0, -5, 0);
+        
+        for(int i=0; i< poolAmount; i++)
+        {
+            spawnpos.y += Random.Range(minmY, maxmY);
+            spawnpos.x = Random.Range(-width, width);
+            GameObject obj = (GameObject)Instantiate (poolObj, spawnpos, Quaternion.identity);
+            //Debug.Log("Spawn Pos: " + spawnpos);
+            obj.transform.SetParent(this.transform);
+            //obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObj()
+    {
+        for(int i = 0; i < pooledObjects.Count; i++)
+        {
+            if(!pooledObjects [i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+
+        GameObject obj = (GameObject)Instantiate(poolObj);
+        obj.SetActive(false);
+        pooledObjects.Add(obj);
+
+        return obj;
+    }
+}
